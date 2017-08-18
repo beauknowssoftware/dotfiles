17fad14cd9ec793e58017c5f9a506c690d4fd2c6
Beau Bennett <beau.bennett@beauknowssoftware.com>
2017-08-18 03:19:37 -0400

    Brew maintenance on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+# Get brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew upgrade
 
+# Get git
 brew install git
 
+# Get dotfiles
 git clone https://github.com/beauknowssoftware/dotfiles.git
-
 cd dotfiles
 
+# Setup dotfiles
 ./setup-new.sh
+
+brew cleanup
